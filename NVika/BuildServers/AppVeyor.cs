422ac74cdf77f363e6f997c87e75f7e2bfc7f323
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-02-08 11:11:14 +0100

    Add compilation message debug log

--- NVika/BuildServers/AppVeyor.cs
+++ NVika/BuildServers/AppVeyor.cs
@@ -7,6 +7,7 @@ namespace NVika
     internal sealed class AppVeyorBuildServer : IBuildServer
     {
         private readonly Logger _logger;
+        private readonly string _appVeyorAPIUrl;
 
         public string Name
         {
@@ -17,6 +18,7 @@ namespace NVika
         public AppVeyorBuildServer(Logger logger)
         {
             _logger = logger;
+            _appVeyorAPIUrl = Environment.GetEnvironmentVariable("APPVEYOR_API_URL");
         }
 
         public bool CanApplyToCurrentContext()
@@ -24,17 +26,21 @@ namespace NVika
             return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPVEYOR"));
         }
 
-        public void WriteMessage(string message, string category, string details, string filename, string line, string offset, string projectName)
+        public async void WriteMessage(string message, string category, string details, string filename, string line, string offset, string projectName)
         {
             using (var httpClient = new HttpClient())
             {
-                httpClient.BaseAddress = new Uri(Environment.GetEnvironmentVariable("APPVEYOR_API_URL"));
-                _logger.Debug("AppVeyor API url: {0}", httpClient.BaseAddress);
+                httpClient.BaseAddress = new Uri(_appVeyorAPIUrl);
 
-                var responseTask = httpClient.PostAsJsonAsync("api/build/compilationmessages", new { Message = message, Category = category, Details = details, FileName = filename, Line = line, Column = offset, ProjectName = projectName });
-                responseTask.Wait();
-                _logger.Debug("AppVeyor CompilationMessage Response: {0}", responseTask.Result.StatusCode);
-                _logger.Debug(responseTask.Result.Content.ReadAsStringAsync().Result);
+                _logger.Debug("Send compilation message to AppVeyor:");
+                _logger.Debug("Message: {0}", message);
+                _logger.Debug("Category: {0}", category);
+                _logger.Debug("Details: {0}", details);
+                _logger.Debug("FileName: {0}", filename);
+                _logger.Debug("Line: {0}", line);
+                _logger.Debug("Column: {0}", offset);
+                _logger.Debug("ProjectName: {0}", projectName);
+                await httpClient.PostAsJsonAsync("api/build/compilationmessages", new { Message = message, Category = category, Details = details, FileName = filename, Line = line, Column = offset, ProjectName = projectName });
             }
         }
     }
