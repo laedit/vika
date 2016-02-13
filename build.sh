adef3c700f3a8bb5843078e04021688a608b1262
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-02-13 16:31:25 +0100

    Fix travis build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mono ./tools/nuget/nuget.exe "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-mono ./tools/FAKE.exe "$@"
+mono ./tools/nuget.exe "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
+mono ./tools/FAKE/tools/Fake.exe build.fsx "$@"
