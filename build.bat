f08fe7d574611d4fbdfe306bd1e764ba3a3c0edc
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2020-10-19 14:55:25 +0200

    adding format and removing build cmds

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-"tools\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\FAKE\tools\Fake.exe" build/build.fsx "%1"
-
-exit /b %errorlevel%
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mono ./tools/nuget.exe "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-mono ./tools/FAKE/tools/FAKE.exe build/build.fsx "$@"
