58c5561c85d5bd6737353300eb010a5f1d00cfef
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2024-05-05 08:48:20 +0200

    fix chocolatey package

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $packageName = 'NVika'
 $url = 'https://github.com/laedit/Vika/releases/download/v[version]/NVika.win-x86.[version].zip'
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -Checksum '[checksum]' -ChecksumType 'sha256'
