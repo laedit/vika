d7a602dc57370472ac5a6fa1234bf3ad24f3cc43
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2024-05-16 15:01:53 +0200

    native aot does not support win-x86

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $packageName = 'NVika'
-$url = 'https://github.com/laedit/Vika/releases/download/v[version]/NVika.win-x86.[version].zip'
+$url = 'https://github.com/laedit/Vika/releases/download/v[version]/NVika.win-x64.[version].zip'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -Checksum '[checksum]' -ChecksumType 'sha256'
